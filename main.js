f910ae189ffc43344da745bdec352c121a892b10
royweb <xytank@gmail.com>
2021-12-29 14:36:04 -0500

    Add and set up Moralis

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hello");
+
+// Connect to Moralis server
+const serverUrl = "https://9qnksoon7okz.usemoralis.com:2053/server";
+const appId = "ntozI14MGOaGCCPaTsw5ZzKyqzzO8wcpMzalWpLQ";
+Moralis.start({ serverUrl, appId });
