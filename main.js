5a41f9060a663e4cf4159bee0939a01336e1bbf7
royweb <xytank@gmail.com>
2021-12-31 12:55:19 -0500

    Login with Moralis

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 console.log("hello");
 
 // Connect to Moralis server
-const serverUrl = "https://9qnksoon7okz.usemoralis.com:2053/server";
-const appId = "ntozI14MGOaGCCPaTsw5ZzKyqzzO8wcpMzalWpLQ";
-Moralis.start({ serverUrl, appId });
+Moralis.initialize("ntozI14MGOaGCCPaTsw5ZzKyqzzO8wcpMzalWpLQ");
+Moralis.serverURL = "https://9qnksoon7okz.usemoralis.com:2053/server";
+
+login = async () => {
+    await Moralis.Web3.authenticate().then(function (user) {
+        console.log('logged in');
+        console.log(Moralis.User.current());
+    })
+}
+
+document.querySelector('#btn-login').onclick = login;
